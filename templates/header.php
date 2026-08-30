1e1354fbe5a478fdc6b511b3851f87f2632abec0
sukhdevsingh006 <sanghasukhdev478@gmail.com>
2026-08-30 22:19:31 +1200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vision College</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+ <link rel="stylesheet" href="../Css/style.css">
+    
+</head>
 
+<body>
+
+    <header class="navbar">
+
+    <div class="logo">
+        <img src="image/VCL_Light Blue.png">
+    </div>
+
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="courses.html">Courses</a>
+        <a href="contact.html">Contact Us</a>
+        <a href="login.html">Login</a>
+    </nav>
+
+</header>
+
+</body>
+</html>
