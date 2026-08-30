b0493686ba4cb486d81c6723e6748c52f4e1fdaa
sukhdevsingh006 <sanghasukhdev478@gmail.com>
2026-08-30 22:20:37 +1200

    Update db.php

--- Pdo/db.php
+++ Pdo/db.php
@@ -1,1 +1,16 @@
+<?php
 
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "vision_college";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+?>
